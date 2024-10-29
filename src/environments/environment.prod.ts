ad79a4c0d294bfc81e4de2b516686170ba679f61
César Sánchez <cesarinfo10@gmail.com>
2024-10-29 18:02:26 -0300

    Eliminar environment.prod.ts del índice de Git

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  MAPBOX_API_KEY: 'pk.eyJ1IjoiY2VzYXJpbmZvMTAiLCJhIjoiY2t3a21xZGxpMXRyNzJ2cXEzaGdlbzk0aCJ9.KNJrhNqZGjVfbq4072FrAA',
-  production: true,
-  apiUrl: 'http://192.168.1.33/avisaso-service/'
-};
